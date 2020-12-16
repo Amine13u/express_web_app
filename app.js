3487719a81becc859f38dcc366c4df45ae021018
Amine13u <46264764+Amine13u@users.noreply.github.com>
2020-12-16 21:48:39 +0100

    Fixing css bug

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-logger = (req, res, next) => {
-  let myDate = new Date("December 21, 2020 10:15:30");
+const logger = (req, res, next) => {
+  let myDate = new Date("August 19, 1975 20:15:30"); // new Date();
   if (myDate.getDay() === 0 || myDate.getDay() === 6) {
     res.sendFile(__dirname + "/Public/work.html");
   } else if (myDate.getHours() > 8 && myDate.getHours() < 17) {
@@ -13,17 +13,17 @@ logger = (req, res, next) => {
   }
 };
 
-app.use(logger);
+// app.use(logger);
 
-app.get("/", (req, res) => {
+app.get("/", logger, (req, res) => {
   res.sendFile(__dirname + "/Public/index.html");
 });
 
-app.get("/services", (req, res) => {
+app.get("/services", logger, (req, res) => {
   res.sendFile(__dirname + "/Public/services.html");
 });
 
-app.get("/contact", (req, res) => {
+app.get("/contact", logger, (req, res) => {
   res.sendFile(__dirname + "/Public/contact.html");
 });
 
